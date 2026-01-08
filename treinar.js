a490d3089dea3be5844afc79a61aa46e8eaf8751
Cauê Machado <caue.gomes07@gmail.com>
2026-01-08 10:14:55 -0300

    Adiciona exemplos e dicas sobre legibilidade de código em JavaScript

--- treinar.js
+++
@@ -1,9 +0,0 @@
-// "Me explica de um jeito simples o que é um laço for em JavaScript?"
-
-// "Quais exercícios extras posso fazer para treinar estruturas condicionais em JavaScript?"
-
-// "Monte um mini quiz para eu testar meus conhecimentos em lógica de programação."
-
-// "Resuma o que são funções em JavaScript e como usá-las."
-
-// "Cria um desafio pra eu praticar arrays em JavaScript, com a solução."
